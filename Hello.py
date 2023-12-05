be792e6e42eccd0fb4428d0018152eccdefceb5e
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2023-12-05 16:04:26 -0300

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import clear_output from IPython.core.display
+import clear_output
 
 while True:
   usu = st.text_input("Crie seu nome de usuário", key="Usuário")
