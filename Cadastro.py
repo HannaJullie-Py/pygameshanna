5db1f136922a4c1406e25a942c18b63e7ed34a4e
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2023-12-05 16:48:46 -0300

    Update Cadastro.py

--- Cadastro.py
+++ Cadastro.py
@@ -3,6 +3,9 @@ import pandas as pd
 import numpy as np
 import time
 
+st.markdown("#Cadastro")
+st.sidebar.markdown("#Cadastro")
+
 usu = st.text_input('Crie seu nome de usuário')
 pas = st.text_input('Crie sua senha')
    
