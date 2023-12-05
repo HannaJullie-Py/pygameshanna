190480cc7bbd799267b7cba4011468e159c99354
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2023-12-05 16:15:22 -0300

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -2,5 +2,5 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-usu = st.text_input('Crie seu nome de usuário')
-print (usu)
+while True:
+  usu = st.text_input('Crie seu nome de usuário')
