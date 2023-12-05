9bf16d403ff9ccab1d2825d83c459fa542de21c5
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2023-12-05 19:18:11 -0300

    Update Cadastro.py

--- Cadastro.py
+++ Cadastro.py
@@ -3,9 +3,8 @@ import pandas as pd
 import numpy as np
 import time
 
-st.image('https://s4.static.brasilescola.uol.com.br/be/2021/09/usina-tres-gargantas.jpg')
-st.title('ANAPH')
-st.write('** Analisamos o seu consumo de energia elétrica! **')
+st.image('https://s4.static.brasilescola.uol.com.br/be/2021/09/usina-tres-gargantas.jpg', caption='https://brasilescola.uol.com.br/geografia/energia-hidreletrica.htm')
+st.title('ANAPH - Analisamos o seu consumo de energia elétrica!')
 
 usu = st.text_input('Crie seu nome de usuário','Usuário')
 pas = st.text_input('Crie sua senha','Senha')
