6305b8f36edc5d2a9264007a5ba5dfad0fe5e7bd
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2023-12-05 16:37:52 -0300

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -4,9 +4,8 @@ import numpy as np
 import time
 
 apagar = st.empty()
-
-with apagar.container():
-    usu = st.text_input('Crie seu nome de usuário')
-    pas = st.text_input('Crie sua senha')
-    
-apagar.empty()    
+for i in range(220):
+    with apagar.container():
+        usu = st.text_input('Crie seu nome de usuário')
+        pas = st.text_input('Crie sua senha')
+    apagar.empty()    
