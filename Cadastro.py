a1c4c4ab9f912a73aaac896ebb0dfd549a92c33f
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2023-12-05 18:29:52 -0300

    Update Cadastro.py

--- Cadastro.py
+++ Cadastro.py
@@ -3,9 +3,12 @@ import pandas as pd
 import numpy as np
 import time
 
+butao = st.button('Criar')
+
 usu = st.text_input('Crie seu nome de usuário','Usuário')
 pas = st.text_input('Crie sua senha','Senha')
 
-if st.button('Criar'):
+if butao:
   print ('Conta criada :D')
+  usu and pas
 
