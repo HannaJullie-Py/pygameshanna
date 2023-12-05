a3485d81602ce2635dcbb984f3bc7090d5e771ee
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2023-12-05 16:25:38 -0300

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -2,10 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import time
-import clear_output
 
-usu = st.text_input('Crie seu nome de usuário')
-pas = st.text_input('Crie sua senha')
-if usu and pas:
-    while True:
-        clear_output(True)
+log = ['usuario','senha']
+
+for seconds in range(220):
+    usu = st.text_input('Crie seu nome de usuário')
+    pas = st.text_input('Crie sua senha')
+    
+    
