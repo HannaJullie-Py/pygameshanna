b55e3152cd56362d791c0f7889d1d464268aacfe
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2023-12-05 18:26:01 -0300

    Update Cadastro.py

--- Cadastro.py
+++ Cadastro.py
@@ -7,5 +7,5 @@ usu = st.text_input('Crie seu nome de usuário','Usuário')
 pas = st.text_input('Crie sua senha','Senha')
 
 if st.button('Criar'):
-                  print ('Conta criada :D')
+  print ('Conta criada :D')
 
