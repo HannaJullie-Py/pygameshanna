6899a4d48a9a59dea923f31045a8ab768524b44d
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2023-12-05 16:21:42 -0300

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+import time
+import clear_output
 
 usu = st.text_input('Crie seu nome de usuário')
 pas = st.text_input('Crie sua senha')
