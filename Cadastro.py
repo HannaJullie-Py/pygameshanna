961f928d2513b6374c31e9e186340906f521708a
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2023-12-05 19:19:23 -0300

    Update Cadastro.py

--- Cadastro.py
+++ Cadastro.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import time
 
-st.image('https://s4.static.brasilescola.uol.com.br/be/2021/09/usina-tres-gargantas.jpg', caption='https://brasilescola.uol.com.br/geografia/energia-hidreletrica.htm')
+st.image('https://s4.static.brasilescola.uol.com.br/be/2021/09/usina-tres-gargantas.jpg',width= 1800, caption='https://brasilescola.uol.com.br/geografia/energia-hidreletrica.htm')
 st.title('ANAPH - Analisamos o seu consumo de energia elétrica!')
 
 usu = st.text_input('Crie seu nome de usuário','Usuário')
