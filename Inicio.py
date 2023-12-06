bdcc9d5f6cc977f5d95a81903ed5166f969ef8c3
mtyzlam <114540964+mtyzlam@users.noreply.github.com>
2023-12-06 15:28:54 -0300

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -12,5 +12,5 @@ st.image('https://i.imgur.com/1P4Ggm3.png',width=700)
 st.write('Analisamos o seu consumo de energia elétrica!')
 
 #Explicação
-st.title('O que é?')
+st.title('Como funciona?')
 st.write(' **Aqui no site da ANAPH é mais fácil calcular o seu consumo de energia, podendo filtrar na data desejada, quando e onde quiser! Crie sua conta no menu CADASTRO para ter acesso completo às funcionalidades do site. Agradecemos a sua visita.** ')
