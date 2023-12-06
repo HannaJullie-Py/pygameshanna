743d6c93eafbd0292e28e1513390bab7ba6d2dfa
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2023-12-06 14:55:13 -0300

    Update Cadastro.py

--- Cadastro.py
+++ Cadastro.py
@@ -5,12 +5,3 @@ import time
 
 st.image('https://s4.static.brasilescola.uol.com.br/be/2021/09/usina-tres-gargantas.jpg',width=700, caption='https://brasilescola.uol.com.br/geografia/energia-hidreletrica.htm')
 st.title('ANAPH - Analisamos o seu consumo de energia elétrica!')
-
-usu = st.text_input('Crie seu nome de usuário')
-pas = st.text_input('Crie sua senha')
-
-butao = st.button('Enter em cada uma das caixas para criar')
-
-if butao:
-  st.write('Conta criada :D')
-
