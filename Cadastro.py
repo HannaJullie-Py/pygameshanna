5ed202abd20bacc53b9b78811743244fd6618bc8
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2023-12-06 14:57:11 -0300

    Update Cadastro.py

--- Cadastro.py
+++ Cadastro.py
@@ -3,5 +3,9 @@ import pandas as pd
 import numpy as np
 import time
 
+st.set_page_config(page_title="ANAPH", page_icon="⚡")
+st.markdown("# ANAPH")
+st.sidebar.header("ANAPH")
+
 st.image('https://s4.static.brasilescola.uol.com.br/be/2021/09/usina-tres-gargantas.jpg',width=700, caption='https://brasilescola.uol.com.br/geografia/energia-hidreletrica.htm')
 st.title('ANAPH - Analisamos o seu consumo de energia elétrica!')
