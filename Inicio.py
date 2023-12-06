eb4f43562dfdd3cdfd07bb15f4cf979ee3780804
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2023-12-06 15:18:27 -0300

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -3,9 +3,10 @@ import pandas as pd
 import numpy as np
 import time
 
-st.header('ANAPH', divider='rainbow')
+
 st.set_page_config(page_title="ANAPH",page_icon="💡")
 st.sidebar.header("ANAPH")
+st.header('ANAPH', divider='rainbow')
 
 st.image('https://i.imgur.com/1P4Ggm3.png',width=700)
 
