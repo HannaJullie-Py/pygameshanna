12843237420105f00aa488b0cbc010c919cc4a7f
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2023-12-06 15:23:09 -0300

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -9,7 +9,8 @@ st.set_page_config(page_title="ANAPH",page_icon="💡")
 st.sidebar.header("ANAPH")
 st.header('ANAPH', divider='rainbow')
 st.image('https://i.imgur.com/1P4Ggm3.png',width=700)
-st.title('Analisamos o seu consumo de energia elétrica!')
+st.write('Analisamos o seu consumo de energia elétrica!')
 
 #Explicação
+st.title('O que é?')
 st.write(' **Boiola** ')
