dfd47328730ce02b2136dff7cdd87e2f747cb4ae
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2023-12-06 15:15:18 -0300

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -6,6 +6,6 @@ import time
 st.set_page_config(page_title="ANAPH",page_icon="💡")
 st.sidebar.header("ANAPH")
 
-st.image('https://s4.static.brasilescola.uol.com.br/be/2021/09/usina-tres-gargantas.jpg',width=700, caption='https://brasilescola.uol.com.br/geografia/energia-hidreletrica.htm')
-st.image('https://i.imgur.com/1P4Ggm3.png', width=200)
+st.image('https://i.imgur.com/1P4Ggm3.png',width=700)
+
 st.title('Analisamos o seu consumo de energia elétrica!')
