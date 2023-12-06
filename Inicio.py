208cccd3d83589fecc01dea372e4ade631b72f86
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2023-12-06 15:04:33 -0300

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import time
 
-st.set_page_config(page_title="ANAPH",page_icon="⚡")
+st.set_page_config(page_title="ANAPH",page_icon="💡")
 st.sidebar.header("ANAPH")
 
 st.image('https://s4.static.brasilescola.uol.com.br/be/2021/09/usina-tres-gargantas.jpg',width=700, caption='https://brasilescola.uol.com.br/geografia/energia-hidreletrica.htm')
