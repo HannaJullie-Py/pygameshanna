3f4f299d0b94dbda6d6ce95ea6eb2623da60a275
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2023-12-06 14:45:13 -0300

    Update 0_Calcular_Consumo.py

--- pages/0_Calcular_Consumo.py
+++ pages/0_Calcular_Consumo.py
@@ -5,3 +5,9 @@ import numpy as np
 st.set_page_config(page_title="Calcular Consumo", page_icon="⚡")
 st.markdown("# Calcular Consumo")
 st.sidebar.header("Calcular Consumo")
+
+opcao = st.selectbox(
+    'Selecione o produto que queira calcular',
+    ('Chuveiro Elétrico', 'Ar Condicionado', 'Ferro de Passar', 'Computador'))
+
+st.write('Você selecionou', opcao)
