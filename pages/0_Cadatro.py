814a20aac62aeaf691b77efb82962b0938947253
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2023-12-06 14:55:37 -0300

    Update 0_Cadatro.py

--- pages/0_Cadatro.py
+++ pages/0_Cadatro.py
@@ -1,1 +1,10 @@
 
+
+usu = st.text_input('Crie seu nome de usuário')
+pas = st.text_input('Crie sua senha')
+
+butao = st.button('Enter em cada uma das caixas para criar')
+
+if butao:
+  st.write('Conta criada :D')
+
