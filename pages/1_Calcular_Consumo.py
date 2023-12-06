2ce6ff27b820883625e6544152bd2901ede00643
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2023-12-06 16:13:55 -0300

    Update 1_Calcular_Consumo.py

--- pages/1_Calcular_Consumo.py
+++ pages/1_Calcular_Consumo.py
@@ -6,8 +6,18 @@ st.set_page_config(page_title="Calcular Consumo", page_icon="⚡")
 st.markdown("# Calcular Consumo")
 st.sidebar.header("Calcular Consumo")
 
+
+
+hd = st.number_input('Digite o tempo de uso diário em horas')
+nd = st.number_input('Digite o numero de dias que usou esse aparelho no mês')
+
 opcao = st.selectbox(
     'Selecione o produto que queira calcular',
     ('Chuveiro Elétrico', 'Ar Condicionado', 'Ferro de Passar', 'Computador'))
+st.caption('Você selecionou', opcao)
 
-st.write('Você selecionou', opcao)
+match opcao
+case 'Chuveiro Elétrico':
+    pot = 5000
+    con = pot*hd*nd/1000
+    st.write(f'Seu gasto mensal será de {con} Kw/h, portanto R$ {0.48*con:.2f}.')
