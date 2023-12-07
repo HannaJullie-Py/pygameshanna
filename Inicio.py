3450aff5758a7b7fcce03dc89dfca84c26a929a8
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2023-12-07 14:48:59 -0300

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -8,9 +8,3 @@ import time
 st.set_page_config(page_title="Py Games by Hanna Jullie",page_icon="🕹️")
 st.sidebar.header("PyHanna")
 st.header('PyHanna', divider='pink')
-st.image('https://i.imgur.com/1P4Ggm3.png',width=700)
-st.write('Analisamos o seu consumo de energia elétrica!')
-
-#Explicação
-st.title('Como funciona?')
-st.write(' **Aqui no site da ANAPH é mais fácil calcular o seu consumo de energia, podendo filtrar na data desejada, quando e onde quiser! Crie sua conta no menu CADASTRO para ter acesso completo às funcionalidades do site. Agradecemos a sua visita.** ')
